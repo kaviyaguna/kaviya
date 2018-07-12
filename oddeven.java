cb14734731289cbd99b8326a16edf4891887507b
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-12 16:06:08 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+public class abc_evenorodd {
+  public static void main(String[] args){
+      int a;
+      System.out.println("enter the number");
+      Scanner cc=new Scanner(System.in);
+       a=cc.nextInt();
+       if(a%2==0){
+           System.out.println("the even number");
+       }
+       else{
+           System.out.println("the odd number");
+       }
+  }
+}
+
