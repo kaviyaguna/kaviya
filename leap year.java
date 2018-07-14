bb92ce4bf28704aa0a4c2f6a51ede7d623a8e837
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-14 15:01:50 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) 
+
+    {
+
+      int a;
+      Scanner in=new Scanner(System.in);
+      a=in.nextInt();
+      if(a%4==0){
+    	  if(a%100==0){
+    		  if(a%400==0){
+      System.out.println("leap year");
+}}}
+      else
+      {
+    	  System.out.println("not a leap year");
+      }
+      
+}
