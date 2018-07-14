ac0a01f9b85303cd540267685b9267a18353b103
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-14 15:23:50 +0530

    Create print two time.java

---
+++ print two time.java
@@ -0,0 +1,9 @@
+ public static void main(String[] args) 
+    {
+      System.out.println("Enter the name");
+      Scanner in=new Scanner(System.in);
+     for(int i=0;i<2;i++){
+    	 System.out.println("hello");
+     }
+      
+}
