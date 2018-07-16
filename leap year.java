77dcfd349951e141e6a71d31e49b5d8ee9c39919
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-16 14:35:47 +0530

    Update leap year.java

--- leap year.java
+++ leap year.java
@@ -6,10 +6,10 @@ public static void main(String[] args)
       Scanner in=new Scanner(System.in);
       a=in.nextInt();
       if(a%4==0){
-    	  if(a%100==0){
-    		  if(a%400==0){
+//if(a%100==0){
+    		//  if(a%400==0){
       System.out.println("leap year");
-}}}
+}
       else
       {
     	  System.out.println("not a leap year");
