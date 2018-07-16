1cd447cb24a5abdd80df2bbadcc0f643b28edc3d
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-16 17:21:58 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,7 @@
+public static void main(String args[]){
+	      String s;
+	      Scanner in=new Scanner(System.in);
+	      s=in.next();
+	      String reverse = new StringBuffer(s).reverse().toString();
+	      System.out.println(reverse);
+	   }
