38eb887126e10a5bdb74fadf26babbc23e64bb05
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-27 11:56:43 +0530

    Rename Odd.java to Oddandeven.java

---
+++ Oddandeven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Odd {
+	public static void main(String args[]){
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		for(int i=1;i<n+1;i++){
+			if(i%2==0 && i%2!=0){
+		}
+			System.out.print(i);
+	}
+	}
+
+	}
