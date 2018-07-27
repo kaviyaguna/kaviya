c288ef7d4d700e8777eaf2d70ef516ddb90b5ee3
kaviyaguna <41111992+kaviyaguna@users.noreply.github.com>
2018-07-27 11:29:33 +0530

    Create Repeate.java

---
+++ Repeate.java
@@ -0,0 +1,19 @@
+public class Repeate{
+public static void main(String args[])
+{
+	int count=0;
+   String s1, s2;
+   Scanner in = new Scanner(System.in);
+   s1 = in.nextLine();
+   s2 = in.nextLine();
+   if (s1==s2){
+	   count++;
+   }
+	   if(count==0){
+      System.out.println("yes");
+   }
+   else{
+      System.out.println("no");
+   }
+}
+}
